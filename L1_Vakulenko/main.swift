a18d9adc9ae683e1d89fa30325e022f6c7784135
RomanVakulenko <vakula_48@mail.ru>
2022-04-28 15:18:28 +0300

    Initial Commit

---
+++ L1_Vakulenko/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  L1_Vakulenko
+//
+//  Created by Roman Vakulenko on 28.04.2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+let number1 = 0.5
+let number2 = 1
+let result: Double = number1 + Double(number2)
+print(result)
